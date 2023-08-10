e4d741ff303b63b09c0fbbaa32221d0d6a7f0815
Amir Hossein Babaeian <babaeian456@gmail.com>
2023-08-10 13:44:38 +0330

    Adding gsn network to hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,13 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require('@nomiclabs/hardhat-waffle');
-// require("@nomicfoundation/hardhat-ethers");
 require("@nomiclabs/hardhat-ganache");
 // require("@nomicfoundation/hardhat-chai-matchers")
 
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    gsn: {
+      url: "http://127.0.0.1:8545",
+    }
+  },
 };
