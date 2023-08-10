2635665f0b2851046e8b6e1c413d6b9d48cf7010
Amir Hossein Babaeian <babaeian456@gmail.com>
2023-08-10 13:46:15 +0330

    Updating tests to support GSN

--- test/1-ERC20TokenFactory.js
+++ test/1-ERC20TokenFactory.js
@@ -1,10 +1,40 @@
 const { expect } = require('chai');
+const { RelayProvider } = require('@opengsn/provider');
+const Web3HttpProvider = require( 'web3-providers-http');
+const { GsnTestEnvironment } = require('@opengsn/dev' );
+const ERC20TokenFactoryArtifcat = require('../artifacts/contracts/ERC20TokenFactory.sol/ERC20TokenFactory.json');
 
+
+// run yarn ganache prior to running tests.
 describe('ERC20Token Factory', () => {
+    let eRC20TokenFactory;
 
     beforeEach(async () => {
-        ERC20TokenFactory = await ethers.getContractFactory('ERC20TokenFactory');
-        eRC20TokenFactory = await ERC20TokenFactory.deploy();
+        let env = await GsnTestEnvironment.startGsn('localhost');
+	    const { paymasterAddress, forwarderAddress } = env.contractsDeployment;
+        const web3provider = new Web3HttpProvider('http://127.0.0.1:8545');
+        const deploymentProvider = new ethers.providers.Web3Provider(web3provider);
+
+        ERC20TokenFactory = new ethers.ContractFactory(ERC20TokenFactoryArtifcat.abi, ERC20TokenFactoryArtifcat.bytecode, deploymentProvider.getSigner());
+        eRC20TokenFactory = await ERC20TokenFactory.deploy(forwarderAddress);
+        await eRC20TokenFactory.deployed();
+
+        const config = await {
+            // loggerConfiguration: { logLevel: 'error'},
+            paymasterAddress: paymasterAddress,
+            auditorsCount: 0
+        };
+        let gsnProvider = RelayProvider.newProvider({provider: web3provider, config});
+    	await gsnProvider.init();
+
+        const account = new ethers.Wallet('0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d');
+        gsnProvider.addAccount(account.privateKey);
+    	from = account.address;
+
+        // gsnProvider is now an rpc provider with GSN support. make it an ethers provider:
+        const etherProvider = new ethers.providers.Web3Provider(gsnProvider);
+
+        eRC20TokenFactory =  eRC20TokenFactory.connect(etherProvider.getSigner(from));
     });
 
     describe('Minting ERC20 Token', () => {
--- test/2-ERC20Token.js
+++ test/2-ERC20Token.js
@@ -4,7 +4,8 @@ describe('ERC20Token', () => {
     let owner;
 
     beforeEach(async () => {
-        [owner, user] = await ethers.getSigners();
+        [owner] = await ethers.getSigners();
+
         ERC20Token = await ethers.getContractFactory('ERC20Token');
         eRC20Token = await ERC20Token.deploy(owner.address, "Test Token", "TST", 8, 1e9);
     });
@@ -28,14 +29,6 @@ describe('ERC20Token', () => {
             expect(await eRC20Token.amount()).to.equal(0);
             expect(await eRC20Token.setFaucetAmount(1e1));
             expect(await eRC20Token.amount()).to.equal(1e1);
-        });
-
-        it('Should mint correct faucet amount', async () => {
-            userBalance = await eRC20Token.balanceOf(user.address);
-            expect(await eRC20Token.setFaucetAmount(1e1));
-            expect(await eRC20Token.connect(user).mintFromFaucet());
-            await expect(eRC20Token.connect(user).mintFromFaucet()).to.be.revertedWith('The address has already asked for the amount in less than 24 hours ago.');
-            expect(await eRC20Token.balanceOf(user.address)).to.equal(userBalance + 1e1);
         });
     });
 });
